a59f1a4ef48bc0907cc54b08cfa8b3053b215004
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-11-21 17:15:18 +0000

    Working well

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,34 +1,44 @@
+# 1 "/var/folders/96/v_l40xh56t74ymwrdnrq84d80000gn/T/tmphtrfbiq3"
+#include <Arduino.h>
+# 1 "/Users/mark/projects/lnbits/LNbits-TPoS-Printer/src/main.ino"
 #include <WiFi.h>
 #include <SPIFFS.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include "Adafruit_Thermal.h"
 
-#define TX_PIN 15 // Arduino transmit  YELLOW WIRE  labeled RX on printer
-#define RX_PIN 2 // Arduino receive   GREEN WIRE   labeled TX on printer
+#define TX_PIN 15
+#define RX_PIN 2
 
-HardwareSerial mySerial(1); // Declare HardwareSerial obj first
-Adafruit_Thermal printer(&mySerial);     // Pass addr to printer constructor
+HardwareSerial mySerial(1);
+Adafruit_Thermal printer(&mySerial);
 
 const char* ssid = "PLUSNET-MWC9Q2";
 const char* password = "4NyMeXtNcQ6rqP";
 
 const char* apiUrl = "https://legend.lnbits.com/api/v1/payments/paginated?limit=10&offset=0&sortby=time&direction=desc";
-const char* apiKey = "a5e68ac0d7f64711a863c5195f664687"; // Replace with your actual API key
+const char* apiKey = "a5e68ac0d7f64711a863c5195f664687";
 
 long lastPrintedPaymentTime = 0;
 bool hasReceiptToPrint = false;
 
 JsonVariant lastPayment;
-
-
+void makeHttpRequest();
+void printConnectedToWifi();
+void printWelcomeReceipt();
+void deserializeAndCompare(String json);
+String unixTimeStampToDateTime(long timestamp);
+void printReceipt();
+void setup();
+void loop();
+#line 25 "/Users/mark/projects/lnbits/LNbits-TPoS-Printer/src/main.ino"
 void makeHttpRequest() {
   HTTPClient http;
   http.begin(apiUrl);
   http.addHeader("X-API-Key", apiKey);
 
   int httpResponseCode = http.GET();
-  
+
   if (httpResponseCode > 0) {
     String payload = http.getString();
     deserializeAndCompare(payload);
@@ -60,7 +70,7 @@ void printWelcomeReceipt() {
 ************************
     Welcome to LNbits 
     )";
-  
+
     printer.wake();
     printer.print(receipt);
     printer.sleep();
@@ -70,7 +80,7 @@ void deserializeAndCompare(String json) {
   DynamicJsonDocument doc(4096);
   deserializeJson(doc, json);
 
-  // loop around the doc["data"] item
+
   for (JsonVariant value : doc["data"].as<JsonArray>()) {
     long time = value["time"].as<long>();
     String paymentHash = value["payment_hash"].as<String>();
@@ -84,7 +94,7 @@ void deserializeAndCompare(String json) {
       tag == "tpos"
       &&
       memo.indexOf("tip") == -1
-      && 
+      &&
       time > lastPrintedPaymentTime
     ) {
       lastPrintedPaymentTime = time;
@@ -92,7 +102,7 @@ void deserializeAndCompare(String json) {
       lastPayment = value;
       hasReceiptToPrint = true;
 
-      // Save last payment hash to SPIFFS
+
       File file = SPIFFS.open("/lastPaymentTime.txt", FILE_WRITE);
       if (!file) {
         Serial.println("Failed to open file for writing");
@@ -109,20 +119,20 @@ void deserializeAndCompare(String json) {
   }
 }
 
-/**
- * @brief Convert a timestamp to Month, 20th 2021 at 12:00pm
- * 
- * @param timestamp 
- * @return String 
- */
+
+
+
+
+
+
 String unixTimeStampToDateTime(long timestamp) {
-  // Convert timestamp to Date
+
   struct tm * timeinfo;
   timeinfo = localtime(&timestamp);
   char date[20];
   strftime(date, 20, "%b %d %Y", timeinfo);
 
-  // Convert timestamp to Time
+
   char time[20];
   strftime(time, 20, "%I:%M%p", timeinfo);
 
@@ -130,9 +140,9 @@ String unixTimeStampToDateTime(long timestamp) {
   return dateTime;
 }
 
-// print receipt
+
 void printReceipt() {
-  // if lastPayment is not empty
+
 
    String receipt = R"(
 
@@ -170,7 +180,7 @@ Fee:   $$fee sats
     String paymentHash = lastPayment["payment_hash"].as<String>();
     int fee = lastPayment["fee"].as<int>();
 
-    // print all the above to serial
+
     Serial.println("Time: " + time);
     Serial.println("Memo: " + memo);
     Serial.println("Amount: " + String(amountMSats));
@@ -179,7 +189,7 @@ Fee:   $$fee sats
     Serial.println("Payment hash: " + paymentHash);
     Serial.println("Fee: " + String(fee));
 
-    // search and replace in receipt
+
     receipt.replace("$$time", time);
     receipt.replace("$$memo", memo);
     receipt.replace("$$amount", String(amountMSats / 1000));
@@ -188,12 +198,12 @@ Fee:   $$fee sats
     receipt.replace("$$fee", String(fee));
 
 
-    // printer.doubleHeightOn();
+
     printer.wake();
     printer.print(receipt);
     printer.sleep();
 
-    // printer.doubleHeightOff();
+
   }
   hasReceiptToPrint = false;
 }
@@ -202,11 +212,11 @@ Fee:   $$fee sats
 void setup() {
   Serial.begin(115200);
 
-  mySerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN); // Initialize serial
-  printer.begin();        // Init printer (same regardless of serial type)
+  mySerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+  printer.begin();
   printWelcomeReceipt();
-  
-  // Connect to WiFi
+
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -216,13 +226,13 @@ void setup() {
   printConnectedToWifi();
 
 
-  // Initialize SPIFFS
+
   if (!SPIFFS.begin(true)) {
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
 
-  // Read last payment hash
+
   File file = SPIFFS.open("/lastPaymentTime.txt", FILE_READ);
   if (file) {
     String lastPrintedPaymentTimeString = file.readString();
