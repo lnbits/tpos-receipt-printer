250d35db9c6774885b08526440562e7d98dcb1a1
blackcoffeexbt <87530449+blackcoffeexbt@users.noreply.github.com>
2023-11-24 14:57:27 +0000

    Added kicad designs and moved dirs

---
+++ firmware/src/config.h
@@ -0,0 +1,11 @@
+#ifndef config_h
+#define config_h
+
+const bool includeQuotes = true; // Add Satoshi quotes to the receipt
+const char* companyName = "LNbits Shop";
+const char* ssid = "PLUSNET-MWC9Q2";
+const char* password = "4NyMeXtNcQ6rqP";
+const char* host = "legend.lnbits.com";
+const char* walletId = "6db1907839e140d882ee6dd698e1d6d9";
+
+#endif
